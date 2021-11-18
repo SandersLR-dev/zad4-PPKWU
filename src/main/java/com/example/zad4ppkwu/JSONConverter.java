2b6b09e5a336e64c970f8a011d52f57b12c47843
Dominik Snopek <222572@edu.p.lodz.pl>
2021-11-18 15:58:34 +0100

    create JSON converter

---
+++ src/main/java/com/example/zad4ppkwu/JSONConverter.java
@@ -0,0 +1,17 @@
+package com.example.zad4ppkwu;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.CDL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
+
+import java.util.Map;
+
+public class JSONConverter {
+
+
+
+
+}
