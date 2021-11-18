16c93a808d817c76edfd552e178b72f8635a6279
Dominik Snopek <222572@edu.p.lodz.pl>
2021-11-18 16:04:07 +0100

    create endpoint for TXT format

---
+++ src/main/java/com/example/zad4ppkwu/Controller/FormatController.java
@@ -0,0 +1,69 @@
+package com.example.zad4ppkwu.Controller;
+
+import com.example.zad4ppkwu.JSONConverter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.json.XML;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RestController
+public class FormatController {
+
+
+    @GetMapping(value = "/TXT/{from}/{text}", produces = {"text/plain"})
+    public String txtConverter(@PathVariable String from, @PathVariable String text) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        if (from.equals("JSON")) {
+
+            Map json = restTemplate.getForObject("http://localhost:8081/" + from + "/" + text, Map.class);
+
+            JSONConverter converter = new JSONConverter();
+
+            return converter.toTXT(json);
+
+        } else if (from.equals("CSV")) {
+
+            String csv = restTemplate.getForObject("http://localhost:8081/" + from + "/" + text, String.class);
+
+            String[] sp = csv.split("\n");
+
+            String[] key = sp[0].split(",");
+            String[] value = sp[1].split(",");
+
+            String json = "{";
+            for (int i = 0; i < key.length; i++) {
+                if (i != 0) {
+                    json += ",";
+                }
+                json += "\"" + key[i] + "\":\"" + value[i] + "\"";
+            }
+            json += "}";
+
+            JSONConverter converter = new JSONConverter();
+
+            return converter.toTXT(json);
+
+        } else if (from.equals("XML")) {
+
+            String xml = restTemplate.getForObject("http://localhost:8081/" + from + "/" + text, String.class);
+            JSONObject xmlJSONObj = XML.toJSONObject(xml);
+            String json = xmlJSONObj.get("root").toString();
+            JSONConverter converter = new JSONConverter();
+
+            return converter.toTXT(json);
+        } else if (from.equals("TXT")) {
+            return restTemplate.getForObject("http://localhost:8081/" + from + "/" + text, String.class);
+        } else {
+            return "BAD FORMAT";
+        }
+    }
+
+
+
+}
