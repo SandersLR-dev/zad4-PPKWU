3ad614fc39beb71544f02ad3f0344d79d085a7c5
Dominik Snopek <222572@edu.p.lodz.pl>
2021-11-18 15:57:35 +0100

    initial commit

---
+++ src/main/java/com/example/zad4ppkwu/Zad4PpkwuApplication.java
@@ -0,0 +1,13 @@
+package com.example.zad4ppkwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad4PpkwuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zad4PpkwuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zad4ppkwu/Zad4PpkwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zad4ppkwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad4PpkwuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
